06115ece39c41e987066c56ba8317a7d41e95e73
Sunbula <khasanovasumbula@gmail.com>
2021-11-27 20:46:56 +0500

    l

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
 	"net"
 	"net/http"
 	"os"
+
+	"github.com/NeverlandMJ/http/cmd/app"
+	"github.com/NeverlandMJ/http/pkg/banners"
 )
 
 func main() {
@@ -18,54 +19,14 @@ func main() {
 }
 
 func execute(host string, port string) (err error) {
+	mux := http.NewServeMux()
+	bannerSvc := banners.NewService()
+	server := app.NewServer(mux, bannerSvc)
+	server.Init()
 	srv := &http.Server{
-		Addr: net.JoinHostPort(host, port),
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			log.Print(r.RequestURI) // full URI
-			log.Print(r.Method)     // method
-
-			body, err := ioutil.ReadAll(r.Body) // request body
-			if err != nil {
-				log.Print(err)
-			}
-			log.Printf("%s", body)
-
-		}),
+		Addr:    net.JoinHostPort(host, port),
+		Handler: mux,
 	}
 
 	return srv.ListenAndServe()
 }
-
-// type handler struct {
-// 	mu       *sync.RWMutex
-// 	handlers map[string]http.HandlerFunc
-// }
-
-// func (h *handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-// 	h.mu.RLock()
-// 	handler, ok := h.handlers[request.URL.Path]
-// 	h.mu.RUnlock()
-
-// 	if !ok {
-// 		http.Error(writer, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-// 		return
-// 	}
-
-// 	handler(writer, request)
-
-// }
-
-// func execute(host string, port string) (err error) {
-// 	mux := http.NewServeMux()
-// 	bannersSvc := banners.NewService()
-
-// 	server := app.NewServer(mux, bannersSvc)
-// 	server.Init()
-
-// 	srv := &http.Server{
-// 		Addr:    net.JoinHostPort(host, port),
-// 		Handler: server,
-// 	}
-// 	log.Print("server start" + host + ":" + port)
-// 	return srv.ListenAndServe()
-// }
